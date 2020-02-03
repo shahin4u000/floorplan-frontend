75ed05cf42f642d6bf73485274a5f4dbe7a5970f
Kazi <Kazi.shahinbd@gmail.com>
2020-02-03 17:06:54 +0100

    csv read done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
+  fileToUpload: File = null;
   title = 'floor-plan';
+  csvContent: any;
+
+  handleFileInput(files: FileList) {
+    this.fileToUpload = files.item(0);
+  }
+
+
+  upload(input: HTMLInputElement) {
+
+    const files = input.files;
+    var content = this.csvContent;
+
+    if (files && files.length) {
+
+      const fileToRead = files[0];
+
+      const fileReader = new FileReader();
+      fileReader.onload = (event) => {
+        let fileContent = (event.target as FileReader).result;
+        console.log(fileContent)
+      }
+
+      fileReader.readAsText(fileToRead, "UTF-8");
+    }
+
+  }
 }
