47eea5c37224be097abf4278ddda965ccfe387e8
Kazi <Kazi.shahinbd@gmail.com>
2020-04-03 20:05:46 +0200

    layout created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   fileToUpload: File = null;
   title = 'floor-plan';
+  fileName:string = "choose file";
   csvContent;
 
 
